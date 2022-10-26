fb857db8161e390430c345bff52dc579e06ff670
JeffersonDevWeb <dalvatechnologies@gmail.com>
2022-10-26 06:28:50 -0300

    path

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ const cors = require('./app/middlewares/cors');
 const routes = require('./routes');
 const errorHandler = require('./app/middlewares/errorHandler');
 
-const port = process.env.PATH || 3001;
-
 const app = express();
 
 app.use(express.json());
@@ -15,4 +13,4 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen(port, () => console.log('🔥 Server started at http://localhost:3001'));
+app.listen('https://restapi-myproducts.herokuapp.com', () => console.log('🔥 Server started at http://localhost:3001'));
