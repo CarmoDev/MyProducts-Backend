2f4c716361f1398baa2164ae6088ca4f6a967485
JeffersonDevWeb <dalvatechnologies@gmail.com>
2022-10-26 06:08:41 -0300

    Corrigindo bugs

--- src/index.js
+++ src/index.js
@@ -13,4 +13,4 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-app.listen('https://git.heroku.com/restapi-myproducts.git', () => console.log('🔥 Server started at http://localhost:3001'));
+app.listen('https://restapi-myproducts.herokuapp.com/', () => console.log('🔥 Server started at http://localhost:3001'));
