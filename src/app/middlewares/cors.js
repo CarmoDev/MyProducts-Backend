bba0130174c70e04c9c3697be4c26b21a008c8d4
JeffersonDevWeb <dalvatechnologies@gmail.com>
2022-10-25 23:16:02 -0300

    first commit

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -3,6 +3,7 @@ module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Methods', '*');
   response.setHeader('Access-Control-Allow-Headers', '*');
   response.setHeader('Access-Control-Max-Age', '5');
+  response.setHeader('Referrer-Policy', 'no-referrer-when-downgrade');
 
   next();
 };
