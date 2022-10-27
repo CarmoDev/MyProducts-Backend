8182fa53a9f3a631f3ab7954d2f68196167d299f
JeffersonDevWeb <dalvatechnologies@gmail.com>
2022-10-27 20:46:32 -0300

    engine

--- src/index.js
+++ src/index.js
@@ -8,8 +8,8 @@ const errorHandler = require('./app/middlewares/errorHandler');
 
 const app = express();
 
-app.use(express.json());
 app.use(cors);
+app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
 
