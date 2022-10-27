dc2d3dd0d1f667fb3eb6fefbd5152f96e4c3e1dc
JeffersonDevWeb <dalvatechnologies@gmail.com>
2022-10-27 20:08:51 -0300

    engine

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(cors);
 app.use(routes);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, (err) => {
   if (err) throw err;
